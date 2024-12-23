028eff2a9465fcb6fb2f2eac874f1e574a3b1965
engsep <smartenergyprojects.eng@gmail.com>
2024-12-23 19:38:48 +0000

    Healthcheck and Node-RED Keycloak fixed

--- verify.sh
+++ verify.sh
@@ -1,12 +1,18 @@
 #|/bin/bash
 
+#apt install jq
+
 echo "Test /orion/version"
 ./scripts/version.sh
+
 echo "Test /orion/ngsi-ld/ex/v1/version"
 ./scripts/versionOrion-LD.sh
+
 echo "Write entity"
 ./scripts/write.sh
+
 echo "Read entity"
 ./scripts/read.sh
+
 echo "Delete entity"
 ./scripts/delete.sh
