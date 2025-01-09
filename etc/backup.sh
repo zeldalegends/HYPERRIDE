a4bb63204beda7f0c48a55eb692d8240b582b847
engsep <smartenergyprojects.eng@gmail.com>
2025-01-09 07:29:13 +0000

    Docker compose file minimized and GUI refined and fixed

---
+++ etc/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\033[1;36mBackup folder: $(pwd)\033[0m"
+read -n 1 -p "Do you want to proceed? [Y/n] " key && [[ -z $key ]] || echo
+[[ $key =~ [nN] ]] && exit
+FILENAME=$(basename $(pwd))_$(date +%Y%m%d%H%M%S)
+tar czvf $FILENAME.tar.gz .
+# tar cz . | openssl enc -aes-256-cbc -e -out $FILENAME.tar.gz.enc
+mkdir public-tmp
+mv $FILENAME.tar.gz public-tmp/
+docker run -d --rm -p 2010:80 --name apache-tmp -v ./public-tmp:/usr/local/apache2/htdocs/ httpd:alpine
+echo -e "\033[1;33mDownload file at: http://$(ip addr show $(ip route | awk '/default/ { print $5 }') | grep 'inet' | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1):2010\033[0m"
+# echo -e "\033[0;33mDecript and uncompress: openssl aes-256-cbc -d -in $FILENAME.tar.gz.enc | tar -xzv\033[0m"
+echo Press Enter to stop the Apache server and delete files...
+read key
+docker stop apache-tmp
+#docker rm apache-tmp
+sudo rm -r public-tmp
---
+++ etc/info.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo -e "\033[1;32mSYSTEM INFORMATION \033[0m"
+KERNEL=$(sudo cat /etc/issue)
+echo ${KERNEL::-6}
+# echo IP: $(curl -s http://whatismyip.akamai.com/)
+# echo IP: $(curl -s https://ipv4.icanhazip.com/)
+# echo IP: $(curl -s https://ifconfig.me/)
+echo IP: $(ip addr show $(ip route | awk '/default/ { print $5 }') | grep 'inet' | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1)
+uname -nrpv
+echo
+echo -e "\033[1;33mCPU \033[0m"
+cat /proc/cpuinfo | grep -E -i -w 'name|cpu MHz'
+echo
+echo -e "\033[1;34mRAM\033[0m"
+free -m
+echo
+echo -e "\033[1;35mDISKS \033[0m"
+df -h
+echo
+read key
+clear
+
+echo -e "\033[1;36mDOCKER CONTAINERS \033[0m"
+docker ps -a --format "table {{.Names}}\t{{.ID}}\t{{.Status}}\t{{.Ports}}\t{{.Image}}" | sort # LC_COLLATE=C sort
+echo
+echo -e "\033[1;30mPORTS \033[0m"
+lsof -i -P -n | grep LISTEN # ss -tunlp
+echo
+echo -e "\033[1;33mCOMMANDS \033[0m"
+echo -e "docker rm -f \$(docker ps -aq) \033[1;32m# Remove all Docker containers \033[0m"
+echo
+echo -e "CTRL+Z \033[1;32m# Put the running process in background \033[0m"
+echo -e "jobs \033[1;32m# List the processes running in background \033[0m"
+echo -e "fg \"%<n>\" \033[1;32m# Recall the <n>th process in background to foreground \033[0m"
+echo
+echo -e "sudo nc -l <port> \033[1;32m# Open a local socket on a specific port \033[0m"
+echo -e "telnet <host> <post> \033[1;32m# Connect to a remote socket on a specific port \033[0m"
+echo
+echo -e "\033[1;34mUSERS \033[0m"
+sudo cat /etc/passwd | grep 100 | grep -v 100:
+echo
+echo -e "\033[1;35mPROCESSES \033[0m"
+ps acrux
+
+read key
+htop
